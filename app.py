0c2887831ba4d17ae790eb3ff236e1b6a8dd67e6
Koushik Anand <koushik2296@gmail.com>
2026-02-06 09:45:19 -0600

    Set page icon for Lookbook AI application

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ from google.api_core import exceptions
 # -----------------------------
 st.set_page_config(
     page_title="LOOKBOOK AI | Global Style Intelligence",
-    page_icon=None,
+    page_icon=" ",
     layout="wide",
     initial_sidebar_state="expanded",
 )
